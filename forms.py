6685a13a74fa6b4326dff87134a805d90b34149c
Sugeng Riyanto <36408012+sugeng-riyanto@users.noreply.github.com>
2024-06-15 18:24:45 +0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Title
+st.title('My first Streamlit app')
+
+# Markdown
+st.markdown('Hello, *World!* :sunglasses:')
+
+# Data
+df = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=['col1', 'col2'])
+
+# Display DataFrame
+st.write(df)
+
+# Line Chart
+st.line_chart(df)
 
 def main():
     st.title('Form using Streamlit')
